f77d1ef51755141d51cc7ea8eec564e94e7c2e34
1aura <l.burrows@elsevier.com>
2018-09-14 13:45:13 +0000

    Laura testing commit from Spring to Kraken

--- elsevierproject/src/main/java/com/qa/ElsevierProjectApplication.java
+++ elsevierproject/src/main/java/com/qa/ElsevierProjectApplication.java
@@ -12,6 +12,7 @@ public class ElsevierProjectApplication {
 		// a random comment
 		//a nother random comment
 		//testing
+		//Laura testing
 		SpringApplication.run(ElsevierProjectApplication.class, args);
 
 		System.out.println("We are merged! :D");
